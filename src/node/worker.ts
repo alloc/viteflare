3d82ba9bf48b81f10c5800e1f774c9b8172cb2f2
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-12-14 14:43:59 -0500

    fix: expose [vars] from wrangler.toml

--- src/node/worker.ts
+++ src/node/worker.ts
@@ -18,6 +18,7 @@ export async function getPreviewToken(bundle: string, config: Config) {
         content: bundle,
       },
       modules: [],
+      variables: config.vars,
     },
     {
       accountId: config.account_id,
